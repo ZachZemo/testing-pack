547fec68803482eed63f59ddef0b56bd90161a95
ZachZemo <zachzemokhol@gmail.com>
2019-11-20 15:01:56 -0500

    first commit

---
+++ packaging_tutorial/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zachzemo", # Replace with your own username
+    version="0.0.1",
+    author="Zach Zemokhol",
+    author_email="zachzemokhol@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZachZemo/testing-pack.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
